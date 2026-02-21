7343e5bea5d25b0e965ae3ff6303edc7284b2535
trcrsired <uwgghhbcad@gmail.com>
2026-02-21 14:15:46 +0800

    remove unit toggle

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // -------------------------------
 //  CONFIG
 // -------------------------------
-const CACHE_NAME = "pwa-geolocation-v17";
+const CACHE_NAME = "pwa-geolocation-v19";
 
 // Files to cache
 const ASSETS = [
