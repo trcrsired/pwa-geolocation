13f6de955ff11f0e92df08e33010c3504e593a01
trcrsired <uwgghhbcad@gmail.com>
2026-02-21 17:20:50 +0800

    try to extend start_window

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // -------------------------------
 //  CONFIG
 // -------------------------------
-const CACHE_NAME = "pwa-geolocation-v25.5";
+const CACHE_NAME = "pwa-geolocation-v25.6";
 
 // Files to cache
 const ASSETS = [
