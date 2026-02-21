a4d996822e64b773bb6a7cbfb4ed3d938eef6a47
trcrsired <uwgghhbcad@gmail.com>
2026-02-21 16:45:04 +0800

    remove queue logic

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // -------------------------------
 //  CONFIG
 // -------------------------------
-const CACHE_NAME = "pwa-geolocation-v25";
+const CACHE_NAME = "pwa-geolocation-v25.1";
 
 // Files to cache
 const ASSETS = [
