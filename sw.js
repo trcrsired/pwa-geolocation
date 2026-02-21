dae047245895fed7afe1b3374f6c5fdfede5de52
trcrsired <uwgghhbcad@gmail.com>
2026-02-21 18:07:37 +0800

    update service worker version

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // -------------------------------
 //  CONFIG
 // -------------------------------
-const CACHE_NAME = "pwa-geolocation-v26";
+const CACHE_NAME = "pwa-geolocation-v27";
 
 // Files to cache
 const ASSETS = [
